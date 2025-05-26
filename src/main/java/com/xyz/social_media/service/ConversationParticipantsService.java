b0cb108aba62926b7bb90997d0bb8e91574bc200
Chandravadhan <m.chandravadhan@gmail.com>
2025-05-26 15:18:59 +0530

    written service logic to set last seen of user

--- src/main/java/com/xyz/social_media/service/ConversationParticipantsService.java
+++ src/main/java/com/xyz/social_media/service/ConversationParticipantsService.java
@@ -2,6 +2,7 @@ package com.xyz.social_media.service;
 
 import com.xyz.social_media.models.ConversationParticipants;
 import com.xyz.social_media.repository.ConversationParticipantRepository;
+import com.xyz.social_media.utilities.UtilityHelper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -22,6 +23,18 @@ public class ConversationParticipantsService {
 
     public List<ConversationParticipants> getParticipantsOfConversation(Long conversationId){
         return conversationParticipantRepository.getParticipantsByConversationId(conversationId);
+    }
+
+    public ConversationParticipants setLastSeen(Long conversationId, Long userId) {
+        ConversationParticipants conversationParticipants =
+                conversationParticipantRepository.findByConversationIdAndUserId(conversationId, userId);
+
+        if (conversationParticipants == null) {
+            throw new RuntimeException("No conversation participant found for conversationId: " + conversationId + " and userId: " + userId);
+        }
+
+        conversationParticipants.setLastSeenAt(UtilityHelper.getCurrentMillis());
+        return conversationParticipantRepository.save(conversationParticipants);
     }
 
 }
